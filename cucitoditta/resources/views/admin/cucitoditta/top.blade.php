f48e434a5afc8889d965701eb35ad860f02cf00f
”Yoshiro <toyoura226@yahoo.co.jp>
2019-12-15 12:54:24 +0000

    first commit

---
+++ cucitoditta/resources/views/admin/cucitoditta/top.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+@section('title', 'topページ')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Cucito ditta</h2>
+            </div>
+        </div>
+    </div>
+@endsection
